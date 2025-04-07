800dba59c57acab8e5ae802bcc1209f1302d4e64
Victoria Tate <151982116+victorialeetate@users.noreply.github.com>
2025-04-07 16:31:04 -0700

    Delete ex2 directory

--- ex2/index.js
+++
@@ -1,6 +0,0 @@
-const Account = require('./accounting');
-
-// Create object from the exported class
-const myAccount = new Account("Jeff");
-myAccount.credit(150);
-console.log(myAccount.describe());
