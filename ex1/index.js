928813822c353f2936ec106bee8521bd49df941e
Victoria Tate <151982116+victorialeetate@users.noreply.github.com>
2025-04-07 16:30:55 -0700

    Delete ex1 directory

--- ex1/index.js
+++
@@ -1,7 +0,0 @@
-const circle = require('./circle');
-
-const radius = 5;
-
-console.log(`For a circle with radius ${radius}:`);
-console.log(`Circumference: ${circle.circumference(radius)}`);
-console.log(`Area: ${circle.area(radius)}`);
